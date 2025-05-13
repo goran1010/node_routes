4dec78dec6f1967e17051047be7e314c8c042d85
Goran Jovic <goran1010jovic@gmail.com>
2025-05-13 11:53:47 +0200

    Fix 404 link in express

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ app.get("/contact-me.html", async (req, res) => {
   let data = await fs.readFile("contact-me.html", "utf8");
   res.status(200).type("html").send(data);
 });
+app.use(async (req, res) => {
+  try {
+    const data = await fs.readFile("404.html", "utf8");
+    res.status(404).type("html").send(data);
+  } catch (err) {
+    res.status(404).send("404 - Page Not Found");
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`node server started at http://localhost:${PORT}`);
